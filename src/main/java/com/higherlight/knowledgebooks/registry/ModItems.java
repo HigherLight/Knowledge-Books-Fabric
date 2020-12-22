b42985d230abd5e79c2da98df985ff593569d5fd
HigherLight <higherlightstudios@gmail.com>
2020-12-22 12:52:19 +0200

    Uploaded whole project cuz y not

--- src/main/java/com/higherlight/knowledgebooks/registry/ModItems.java
+++ src/main/java/com/higherlight/knowledgebooks/registry/ModItems.java
@@ -1,6 +1,7 @@
 package com.higherlight.knowledgebooks.registry;
 
 import com.higherlight.knowledgebooks.KnowledgeBooks;
+import net.minecraft.item.FoodComponent;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
@@ -8,7 +9,7 @@ import net.minecraft.util.registry.Registry;
 
 public class ModItems {
 
-    public static final Item BOOK_OF_KNOWLEDGE = new Item(new Item.Settings().group(ItemGroup.TOOLS));
+    public static final Item BOOK_OF_KNOWLEDGE = new Item(new Item.Settings().group(ItemGroup.TOOLS).maxCount(1));
 
     public static void registerItems() {
         Registry.register(Registry.ITEM, new Identifier(KnowledgeBooks.MOD_ID, "book_of_knowledge"), BOOK_OF_KNOWLEDGE);
